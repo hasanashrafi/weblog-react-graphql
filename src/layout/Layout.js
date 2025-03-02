fefd6db4211590107eca59bd22f7ffdbd554873f
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-02 11:34:47 +0330

    `feat: updated ButtonAppBar component with new functionality and design changes`

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -8,25 +8,40 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
 export default function ButtonAppBar() {
+  const [isOpen, setIsOpen] = React.useState(false)
+
   return (
     <Box sx={{ flexGrow: 1 }} >
       <AppBar position="static">
         <Toolbar>
-          <IconButton
+          <IconButton onClick={() => setIsOpen(!isOpen)}
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-            <MenuIcon />
+            <MenuIcon className='' />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            Dev's Weblog
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Typography color="inherit">iDeal</Typography>
         </Toolbar>
       </AppBar>
+      {
+        isOpen ? (
+          <div className='flex flex-col  shadow-lg rounded max-h-max w-full p-4 bg-white/20 border backdrop-blur-md'>
+            <ul className='flex flex-col gap-4'>
+              <li>home</li>
+              <li>Blogs</li>
+              <li>Comments</li>
+              <li>Authors</li>
+            </ul>
+          </div>
+        ) :
+          null
+      }
     </Box>
   );
 }
