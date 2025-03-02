45bf36820ebb3016551fc9c313677ae8ffd1ec68
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-02 10:55:15 +0330

    `Added Apollo Client and InMemoryCache for GraphQL data management`

--- src/index.js
+++ src/index.js
@@ -3,15 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://us-east-1-shared-usea1-02.cdn.hygraph.com/content/cls621f810gn201w3gd57bl3o/master',
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
+  </ApolloProvider>
+ 
   </React.StrictMode>
-);
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
