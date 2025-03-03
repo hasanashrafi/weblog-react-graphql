1968202082240deb6906fdb134c0b2e151ed74c7
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-03 18:45:28 +0330

    added fonts configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
- /** @type {import('tailwindcss').Config} */
- export default {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "YekanBakhBold": "YekanBakh Bold",
+        "YekanBakhFat": "YekanBakh Fat",
+        "YekanBakhHeavy": "YekanBakh Heavy",
+        "YekanBakhLight": "YekanBakh Light",
+        "YekanBakhMedium": "YekanBakh Medium",
+        "YekanBakhRegular": "YekanBakh Regular",
+      }
+    },
   },
   plugins: [],
 }
