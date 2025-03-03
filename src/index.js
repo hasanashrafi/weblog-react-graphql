3fc6302edd4edbca2214bc596e96b37b61297301
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-03 19:16:06 +0330

    added theme provider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ThemeProvider } from '@mui/material';
+import theme from './mui/theme';
 
 const client = new ApolloClient({
   uri: 'https://us-east-1-shared-usea1-02.cdn.hygraph.com/content/cls621f810gn201w3gd57bl3o/master',
@@ -11,13 +13,15 @@ const client = new ApolloClient({
   connectToDevTools: true,
 });
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
- 
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </ApolloProvider>
   </React.StrictMode>
 )
 
