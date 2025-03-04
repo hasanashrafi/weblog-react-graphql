564087297ebb760496b7db55601bbd1b7182b833
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-04 22:57:25 +0330

    get posts with graphql

--- src/components/blogs/Blogs.js
+++ src/components/blogs/Blogs.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import { useQuery } from '@apollo/client'
+import React, { useState, useEffect } from 'react'
+import { GET_BLOGS_INFO } from '../../graphql/queries'
+import { Grid2 } from '@mui/material'
 
 function Blogs() {
+  const { loading, data, error } = useQuery(GET_BLOGS_INFO)
+  const [posts, setPosts] = useState([])
+
+  console.log(data)
+
+  useEffect(() => {
+    if (data) {
+      setPosts(data.posts)
+    }
+  }, [data])
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error...</p>
+
   return (
-    <div>Blogs</div>
+    <Grid2 container  spacing={2} padding={3} className=''>
+      {posts.map((post) => (
+        <Grid2 key={post.id} mt={4} size={{ xs: 12,sm:6, md: 4 }}>
+
+          <img src={post.coverPhoto.url} alt='post.title'  />
+          <p >{post.title}</p>
+
+
+        </Grid2>
+      ))}
+    </Grid2>
   )
 }
 
