1b01bab7f7fb308d499d0eaf829c06fa6c5ae036
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-04 22:56:59 +0330

    comment query

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 
-import { gql, useQuery } from '@apollo/client';
+// import { gql, useQuery } from '@apollo/client';
 
 
 import HomePage from './components/home/HomePage';
-import NavBar from './layout/Layout';
 
-const QUERY = gql`
-  query  {
-    authors {
-      name
-    }
-    posts {
-      title
-    }
-  }
-`;
+// const QUERY = gql`
+//   query  {
+//     authors {
+//       name
+//     }
+//     posts {
+//       title
+//     }
+//   }
+// `;
 
 function App() {
-  const { loading, error, data } = useQuery(QUERY);
-  console.log(loading, data, error)
+  // const { loading, error, data } = useQuery(QUERY);
+  // console.log(loading, data, error)
 
   return (
     
