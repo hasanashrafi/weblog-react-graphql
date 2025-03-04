891cb5ebc0b5baefb21d46b35472f717582b9013
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-04 18:25:40 +0330

    added grid box and items

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,17 +1,33 @@
 import React from 'react'
-import ButtonAppBar from '../../layout/Layout'
 import { Container, Grid2, Typography } from '@mui/material'
+import Authors from '../authors/Authors'
+import Blogs from '../blogs/Blogs'
 
 function HomePage() {
     return (
-        <Container maxWidth="lg">
-            <Grid2 container spacing={3}>
-                <Grid2 item xs={12}>
-                    <Typography>نویسنده ها</Typography>
+        <Container maxWidth="lg"  >
+            <Grid2 container spacing={2} padding={3}>
+                <Grid2 mt={4} size={{ xs: 12, md: 3 }}>
+                    <Typography
+                        component="h3"
+                        variant='h5'
+                        mb="3"
+                        fontWeight={700}>
+                        نویسنده ها
+                    </Typography>
+                    <Authors />
                 </Grid2>
 
-                <Grid2 item xs={12}></Grid2>
-                <Typography>مقالات</Typography>
+                <Grid2 mt={4} size={{ xs: 12, md: 3 }}>
+                    <Typography
+                        component="h3"
+                        variant='h5'
+                        mb="3"
+                        fontWeight={700}>
+                        مقالات
+                    </Typography>
+                    <Blogs />
+                </Grid2>
             </Grid2>
         </Container>
     )
