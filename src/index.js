90e85d077bf928faaa22f12cc2a5559aa68c3a81
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-04 16:21:32 +0330

    get uri from .env

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { ThemeProvider } from '@mui/material';
 import theme from './mui/theme';
 
+
 const client = new ApolloClient({
-  uri: 'https://us-east-1-shared-usea1-02.cdn.hygraph.com/content/cls621f810gn201w3gd57bl3o/master',
+  uri: process.env.REACT_APP_GRAPHQL_URI,
   cache: new InMemoryCache(),
   connectToDevTools: true,
 });
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
