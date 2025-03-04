e05682c0881e9f97eab0c5004de4922a7b33762c
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-04 18:47:44 +0330

    create and added items to footer component

---
+++ src/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='flex justify-center items-center text-white font-YekanBakhMedium p-3 text-xl max-w-7xl mx-auto rounded-t-3xl bg-[#1976d3]'>
+            <p>Developed By HsN</p>
+        </div>
+    )
+}
+
+export default Footer
