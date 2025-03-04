d41fcebb2157250e1d7d9d5536699550cf26db94
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-04 22:58:18 +0330

    edit styles

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,10 +1,17 @@
+import { AppBar, Box, Toolbar, Typography } from '@mui/material'
 import React from 'react'
 
 function Footer() {
     return (
-        <div className='flex justify-center items-center text-white font-YekanBakhMedium p-3 text-xl max-w-7xl mx-auto rounded-t-3xl bg-[#1976d3]'>
-            <p>Developed By HsN</p>
-        </div>
+        <Box sx={{ flexGrow: 1 }} maxWidth="lg" mx="auto"   >
+            <AppBar position="sticky" className='rounded-t-3xl' height={1} >
+                <Toolbar className=' flex items-center justify-center text-center '  >
+                    <Typography  variant="h6" mx="auto" component="div" sx={{ flexGrow: 1 }}>
+                        Developed By HsN
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
     )
 }
 
