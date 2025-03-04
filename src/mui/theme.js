3d625c46b054e4a25fd9eec43da4364835fd5589
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-04 18:26:02 +0330

    `Update theme.js: change font family to YekanBakhMedium, add direction property`

--- src/mui/theme.js
+++ src/mui/theme.js
@@ -1,14 +1,15 @@
 import { createTheme } from "@mui/material";
 
-const theme =  createTheme({
-    typography:{
-        fontFamily: `"YekanBakhRegular","Roboto","Arial"`,
-        fontWeightLight:300,
-        fontWeightRegular:400,
-        fontWeightMedium:500,
-        fontWeightBold:700,
-        fontWeightHeavy:800,
-        fontWeightFat:900,
-    }
+const theme = createTheme({
+    typography: {
+        fontFamily: `"YekanBakhMedium","Roboto","Arial"`,
+        fontWeightLight: 300,
+        fontWeightRegular: 400,
+        fontWeightMedium: 500,
+        fontWeightBold: 700,
+        fontWeightHeavy: 800,
+        fontWeightFat: 900,
+    },
+    direction: "rtl",
 })
 export default theme;
