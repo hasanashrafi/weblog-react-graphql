b360eef6f87c5ad9426ebbf3efcdd10e8157bc6d
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-05 22:50:05 +0330

    create blog component

---
+++ src/components/blogs/BlogPage.js
@@ -0,0 +1,12 @@
+import { Grid } from '@mui/material'
+import React from 'react'
+
+function BlogPage() {
+    return (
+        <Grid container maxWidth="lg" padding={2} className='min-h-screen'>
+            BlogPage
+        </Grid>
+    )
+}
+
+export default BlogPage
