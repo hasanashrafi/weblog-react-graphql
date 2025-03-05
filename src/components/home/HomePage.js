c2ffef7029e3eb8baf61026aa90dafb576f2a2ef
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-05 22:50:32 +0330

    edit styles

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -7,26 +7,31 @@ function HomePage() {
     return (
         <Container maxWidth="lg" className='min-h-screen' >
             <Grid container spacing={2}  >
-                <Grid item mt={4} sx={12} md={3}>
+                <Grid item mt={4} sx={12} md={3} >
                     <Typography
                         component="h3"
                         variant='h5'
-                        mb="3"
+                        mb={2}
+                       sx={{textShadow:"0px 0px 6px rgba(49,41,45,0.4)"}}
                         fontWeight={700}>
                         نویسنده ها
                     </Typography>
                     <Authors />
+
                 </Grid>
 
                 <Grid item mt={4} sx={12} md={9} >
                     <Typography
                         component="h3"
                         variant='h5'
-                        mb="3"
+                        mb={2}
+                        sx={{textShadow:"0px 0px 6px rgba(49,41,45,0.4)"}}
+
                         fontWeight={700}>
                         مقالات
                     </Typography>
                     <Blogs />
+
                 </Grid>
             </Grid>
         </Container>
