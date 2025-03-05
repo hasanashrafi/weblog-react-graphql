34efab5afb5f845a03e41034a8aba268ce44a9f2
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-05 22:50:18 +0330

    edit styles

--- src/components/blogs/Blogs.js
+++ src/components/blogs/Blogs.js
@@ -1,7 +1,7 @@
 import { useQuery } from '@apollo/client'
 import React, { useState, useEffect } from 'react'
 import { GET_BLOGS_INFO } from '../../graphql/queries'
-import { Grid, Typography } from '@mui/material'
+import { Grid } from '@mui/material'
 import CardEl from '../../shared/CardEl'
 
 function Blogs() {
@@ -14,14 +14,14 @@ function Blogs() {
     }
   }, [data])
 
-  if (loading) return <Typography>Loading...</Typography>
-  if (error) return <Typography>Error...</Typography>
+  if (loading) return <div className='flex items-center justify-center min-h-screen'>در حال بارگذاری مقالات</div>
+  if (error) return <div className='flex items-center justify-center min-h-screen'> {error.message}مشکلی پیش آمده است</div>
 
   return (
     <Grid container spacing={2} >
       {posts.map((post) => (
-        <Grid item xs={12} sm={6} md={4} >
-          <CardEl key={post.id} post={post} />
+        <Grid item xs={12} sm={6} md={4} key={post.id}>
+          <CardEl  post={post} />
         </Grid>
       ))}
     </Grid>
