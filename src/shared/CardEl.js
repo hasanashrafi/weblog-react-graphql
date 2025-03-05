3802f7af10267740b729c22a7da945981f38c500
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-05 16:32:37 +0330

    `Added Button and Divider components to CardEl.js`

--- src/shared/CardEl.js
+++ src/shared/CardEl.js
@@ -1,13 +1,14 @@
-import { Avatar, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from '@mui/material'
+import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Divider, Typography } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function CardEl({ post }) {
     console.log(post)
     return (
-        <Card sx={{  borderRadius:"15px" ,boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px" }}>
+        <Card sx={{ borderRadius: "15px", boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px" }}>
             <CardHeader
-                avatar={<Avatar sx={{marginLeft:2}} 
-                src={post.author.avatar.url} />}
+                avatar={<Avatar sx={{ marginLeft: 2 }}
+                    src={post.author.avatar.url} />}
                 title={post.author.name}
             />
 
@@ -15,13 +16,17 @@ function CardEl({ post }) {
                 component="img"
                 image={post.coverPhoto.url}
                 alt={post.title}
-               sx={{height:200}}
+                sx={{ height: 200 }}
             />
             <CardContent >
-                <Typography variant="h6" gutterBottom>{post.title}</Typography>
+                <Typography component="h3" variant="h6" color='text-primary' fontWeight={600} gutterBottom>{post.title}</Typography>
             </CardContent>
+            <Divider variant='middle' sx={{ margin: "10px" }} />
             <CardActions>
-                
+            {/* <Link to={`/blogs/${post.slug}` } style={{textDecoration:"none", width:"100%"}}  >  */}
+                <Button variant='outlined' size='small' sx={{width:"100%", borderRadius:3}}>مطالعه مقاله</Button>
+            {/* </Link> */}
+          
             </CardActions>
         </Card>
 
