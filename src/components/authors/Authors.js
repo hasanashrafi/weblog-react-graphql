aa77bbe8ee0512d6f51ada18c1410c7fbee78266
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-05 17:09:54 +0330

    handle authors component and get authors list with useQuery and props to author element

--- src/components/authors/Authors.js
+++ src/components/authors/Authors.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import { useQuery } from '@apollo/client'
+import React, { useState, useEffect } from 'react'
+import { GET_AUTHORS_INFO } from '../../graphql/queries'
+import { Grid } from '@mui/material'
+import AuthorEl from '../../shared/AuthorEl'
 
 function Authors() {
+  const { loading, data, error } = useQuery(GET_AUTHORS_INFO)
+  const [authors, setAuthors] = useState([])
+
+  useEffect(() => {
+    if (data) {
+      setAuthors(data.authors)
+    }
+  }, [data])
+
+  if (loading) return <div className='min-h-screen flex items-center justify-center'>Loading...</div>
+  if (error) return <div className='min-h-screen flex items-center justify-center'>Error: {error.message}</div>
+
+  console.log(data)
   return (
-    <div>Authors</div>
+    <Grid container
+      sx={{
+        borderRadius: "15px",
+        boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px"
+      }}>
+
+      {authors.map((author) => (
+        <Grid item xs={12} padding={2}>
+          <AuthorEl key={author.id} author={author} />
+        </Grid>
+      ))}
+    </Grid>
   )
 }
 
