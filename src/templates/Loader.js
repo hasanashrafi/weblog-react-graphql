c391d66fce98e58c8ad33518b72bfb5c985a507d
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-06 22:42:16 +0330

    create loader component

---
+++ src/templates/Loader.js
@@ -0,0 +1,15 @@
+import { CircularProgress, Container, Stack } from '@mui/material'
+
+import React from 'react'
+
+function Loader() {
+    return (
+        <Container maxWidth="lg" sx={{ display: "flex", alignItems: "center", justifyContent: "center", marginBottom: 2, minHeight: "100vh" }}>
+          <Stack spacing={2} direction="row" alignItems="center">
+            <CircularProgress size="3rem" />
+          </Stack>
+        </Container>
+    )
+}
+
+export default Loader
