d2aa2e4777539ec18ac15c34420c4c8a09c7ba54
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-06 22:41:10 +0330

    edit styles and added loader

--- src/components/blogs/Blogs.js
+++ src/components/blogs/Blogs.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react'
 import { GET_BLOGS_INFO } from '../../graphql/queries'
 import { Grid } from '@mui/material'
 import CardEl from '../../shared/CardEl'
+import Loader from '../../templates/Loader'
 
 function Blogs() {
   const { loading, data, error } = useQuery(GET_BLOGS_INFO)
@@ -14,14 +15,16 @@ function Blogs() {
     }
   }, [data])
 
-  if (loading) return <div className='flex items-center justify-center min-h-screen'>در حال بارگذاری مقالات</div>
   if (error) return <div className='flex items-center justify-center min-h-screen'> {error.message}مشکلی پیش آمده است</div>
 
   return (
     <Grid container spacing={2} >
+      {
+        loading && <Loader />
+      }
       {posts.map((post) => (
         <Grid item xs={12} sm={6} md={4} key={post.id}>
-          <CardEl  post={post} />
+          <CardEl post={post} />
         </Grid>
       ))}
     </Grid>
