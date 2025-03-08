2cc0cdb1a03bf4c95c71fd92e997f52aa35f8434
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-08 17:25:38 +0330

    edit props and styles

--- src/shared/CardEl.js
+++ src/shared/CardEl.js
@@ -2,28 +2,32 @@ import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, 
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-function CardEl({ post }) {
+function CardEl({ title, slug, coverPhoto, author }) {
+
+    console.log(coverPhoto)
 
     return (
-        <Card sx={{ borderRadius: "15px", boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px", }}>
-            <CardHeader
-                avatar={<Avatar sx={{ marginLeft: 2 }}
-                    src={post.author.avatar.url} />}
-                title={post.author.name}
-            />
+        <Card sx={{ height: "450px", borderRadius: "15px", boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px", display: "flex", flexDirection: "column", justifyContent: "space-around" }}>
+            {author && (<Link to={`/authors/${author.slug}`}>
+                <CardHeader
+                    avatar={<Avatar sx={{ marginLeft: 2 }}
+                        src={author.avatar.url} />}
+                    title={author.name}
+                />
+            </Link>)}
 
             <CardMedia
                 component="img"
-                image={post.coverPhoto.url}
-                alt={post.title}
+                image={coverPhoto.url}
+                alt={title}
                 sx={{ height: 200 }}
             />
             <CardContent >
-                <Typography component="h3" variant="h6" color='text-primary' fontWeight={600} gutterBottom>{post.title}</Typography>
+                <Typography component="h3" variant="h6" color='text-primary' fontWeight={600} gutterBottom>{title}</Typography>
             </CardContent>
             <Divider variant='middle' sx={{ margin: "10px" }} />
             <CardActions>
-                <Link to={`/blogs/${post.slug}`} style={{ textDecoration: "none", width: "100%" }}  >
+                <Link to={`/blogs/${slug}`} style={{ textDecoration: "none", width: "100%" }}  >
                     <Button variant='outlined' size='small' sx={{ width: "100%", borderRadius: 3 }}>مطالعه مقاله</Button>
                 </Link>
 
