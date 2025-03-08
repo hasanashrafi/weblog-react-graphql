5dee9e3079e9d14193488f584addad1e3f7bc336
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-08 17:24:43 +0330

    edit styles and items

--- src/components/blogs/BlogPage.js
+++ src/components/blogs/BlogPage.js
@@ -1,12 +1,15 @@
 import { useQuery } from '@apollo/client'
-import { Avatar, CardMedia, Container, Grid, Typography } from '@mui/material'
+import { Avatar, Box, CardMedia, Container, Grid, Typography } from '@mui/material'
 import React from 'react'
 import { GET_POST_INFO } from '../../graphql/queries'
-import { Link, useParams } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import Loader from '../../templates/Loader'
+import sanitizeHtml from 'sanitize-html'
+import { ArrowBack } from '@mui/icons-material'
 
 function BlogPage() {
     const params = useParams()
+    const navigate = useNavigate()
 
     const { loading, data, error } = useQuery(GET_POST_INFO, {
         variables: { slug: params.slug }
@@ -15,48 +18,57 @@ function BlogPage() {
     if (loading) return <Loader />
 
     const { post } = data
-    console.log(post)
+    console.log(data)
+
 
     if (error) return <p>{error.message}</p>
 
     return (
         <Container maxWidth="lg" sx={{ padding: 2, minHeight: '100vh' }}>
+            <Grid container>
 
+                <Grid item
+                    xs={12}
+                    mt={9}
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="space-between">
 
+                    <Typography
+                        color='primary'
+                        fontWeight={900}
+                        sx={{ fontFamily: "YekanBakhHeavy", textAlign: "center", fontSize: "24px" }}>
+                        {post.title}
+                    </Typography>
+                    <ArrowBack className='hover:text-gray-600 transition-all ease-in-out cursor-pointer' onClick={() => navigate(-1)} />
+                </Grid>
 
-            <Grid sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", marginBottom: 2, minHeight: "100vh" }}>
-                <Grid item sx={{ width: "100%", }} >
+                <Grid item mt={4} xs={12} sx={{ width: "100%", }} >
                     <CardMedia
                         component="img"
                         image={post.coverPhoto.url}
                         alt={post.title}
-                        sx={{ height: 400, width: "90%", mx: "auto", borderRadius: "15px", }}
+                        sx={{ height: 500, width: "100%", mx: "auto", borderRadius: "5px", }}
                     />
                 </Grid>
 
-                <Grid item m={5}>
-                    <Typography mb={5} fontWeight={900} sx={{ fontFamily: "YekanBakhHeavy", textAlign: "center", fontSize: "24px" }}>
-                        {post.title}
-                    </Typography>
-                    <Typography sx={{ textAlign: "justify" }}>
-                        {post.content.text}
-                    </Typography>
+                <Grid item xs={12} sx={{ mt: 5, }}>
+                    <Link className='flex items-center' to={`/authors/${post.author.slug}`}>
+                        <Avatar src={post.author.avatar.url} sx={{ width: 80, height: 80 }} />
+                        <Box component="div">
+                            <Typography component="p" variant='h6' fontWeight={600} mr={2}>{post.author.name}</Typography>
+                            <Typography component="p" variant='p' color='text.secondary' mr={2}>{post.author.field}</Typography>
+
+                        </Box>
+                    </Link>
+
                 </Grid>
 
-                <Grid item sx={{ mb: 2, mx: "auto", width: "90%", display: "flex", alignItems: "center", justifyContent: "space-around" }}>
-                    <Grid item sx={{ display: "flex", alignItems: "center", }} >
-                        <Link className='flex items-center' to={`/authors/${post.author.slug}`}>
-                            <Avatar src={post.author.avatar.url} />
-                            <Typography mr={2}>{post.author.name}</Typography>
-                        </Link>
-                    </Grid>
-                    <Typography>
-                        {new Date(post.publishedAt).toLocaleDateString('en-US', {
-                            year: 'numeric',
-                            month: 'long',
-                            day: 'numeric',
-                        })}                    </Typography>
+                <Grid item xs={12} my={5}>
+                    <div className=' text-start' dangerouslySetInnerHTML={{ __html: sanitizeHtml(post.content.html) }}>
+                    </div>
                 </Grid>
+
 
 
             </Grid>
