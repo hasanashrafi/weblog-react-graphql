f5c11a530844030600e8adc0fa12f45beb16fe70
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-08 22:00:18 +0330

    delete file

--- src/helper/textEditor.js
+++
@@ -1,8 +0,0 @@
-
-function formatText(text) {
-    text = text.replace(/\n/g, ' ').replace(/\./g, '.').replace(/\s+/g, ' ').trim();
-    text = '<p>' + text + '</p>';
-    return text;
-}
-
-export default formatText;
