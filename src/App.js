ba9aa4c780c24a806ec8288b5752693d006c636a
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-08 21:18:52 +0330

    added routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./components/home/HomePage";
 import BlogPage from "./components/blogs/BlogPage";
 import AuthorPage from "./components/authors/AuthorPage";
+import Blogs from "./components/blogs/Blogs";
+import Authors from "./components/authors/Authors";
 
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
     <>
 <Routes>
   <Route path="/" element={<HomePage />} /> 
+  <Route path="/blogs" element={<Blogs />} /> 
+  <Route path="/authors" element={<Authors />} /> 
   <Route path="/blogs/:slug" element={<BlogPage />} /> 
   <Route path="/authors/:slug" element={<AuthorPage />} /> 
 </Routes>
